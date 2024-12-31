93d4b8a4bf0f02f4e604afce0254799e4369597b
cr1tii <lostmemories531@gmail.com>
2024-12-31 15:12:27 +0200

    to delete again

--- tgit.py
+++ tgit.py
@@ -1,4 +1,4 @@
-from ric.console import console
+from rich.console import Console
 
-c1 = Consol()
+c1 = Console()
 c1.print(f"by seeing this message that means code worked!",style="bold underline yellow")
