0364491216cd3f932090d8d77e57d152f9d06658
cr1tii <lostmemories531@gmail.com>
2025-04-03 05:01:54 +0200

    alr so gang , i had sum turbs ima cancel one choice from main list , but ima let yall know what it is , i will drop it later as a an update for it , ima keep posting sa7 on its date , get ready yall.

--- freedom.py
+++ freedom.py
@@ -2,7 +2,7 @@ import re
 
 def process_input():
     user_input = input("type what you like sir: ") 
-    list1 = ["circle"]
+    list1 = ["time"]
     if re.search(list1[0],user_input,re.IGNORECASE): 
         print(list1[0]) 
     else: 
